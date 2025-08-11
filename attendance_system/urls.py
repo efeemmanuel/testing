97aebb7bc2ee02ff4016f02465cbcbac46096323
Efe Emmanuel <83638482+efeemmanuel@users.noreply.github.com>
2025-08-11 23:23:41 +0100

    Add files via upload

---
+++ attendance_system/urls.py
@@ -0,0 +1,53 @@
+"""
+URL configuration for attendance_system project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+from django.contrib import admin, auth as auth_views
+from django.urls import path, include
+from core.views import login_view, role_home_redirect
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", include('django.contrib.auth.urls')),
+    # Role‑specific dashboards
+    path("",    include("core.urls",    namespace="core")),
+    path("superadmin/", include("superadmin.urls", namespace="superadmin")),
+    path("principal/",  include("principal.urls",  namespace="principal")),
+    path("teacher/",    include("teacher.urls",    namespace="teacher")),
+
+    # Support tickets (shared)
+    path("support/",    include("support.urls",    namespace="support")),
+
+
+
+    # authentication
+    path("login/",  login_view, name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path('redirect/', role_home_redirect, name='role-home-redirect'),
+
+
+]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+
+
+
+
